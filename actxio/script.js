83c0031d9b03dec45d3a8fe187d2c714eb6217e6
AlexEliasC <aelias@crackthecode.pe>
2022-01-17 10:31:34 -0500

    ultimas sesiones

--- actxio/script.js
+++
@@ -1,5 +0,0 @@
-// Aplicando lo aprendido 2
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
